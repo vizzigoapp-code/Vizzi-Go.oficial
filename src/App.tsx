eed7cb7c95c251c6b88a7c325aa6951be43ba615
Juan Souza <vizzigoapp@gmail.com>
2026-03-22 09:45:32 -0300

    primeira versão vizzi go

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🚀 VIZZI GO</h1>
+      <p>Seu app já está rodando!</p>
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>🚀 VIZZI GO FUNCIONANDO</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
