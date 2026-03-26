2eb22c9b0d6c25b2e0c11f71f90ebf5c568880c8
Juan Souza <vizzigoapp@gmail.com>
2026-03-26 20:32:13 -0300

    fix: tipagem completa do env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <tipos de referência="vite/client" />
+
+interface ImportMetaEnv {
+leituraVITE_SUPABASE_URL: string
+leitura VITE_SUPABASE_ANON_KEY: string
+}
+
+interface ImportMeta {
+leitura env: ImportMetaEnv
+}
